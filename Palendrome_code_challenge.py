e0527af9673ec6603ecbf081d8732e52da230748
joel wood <joelww1556@gmail.com>
2025-10-19 20:32:45 +1300

    Update Palendrome_code_challenge.py

--- Palendrome_code_challenge.py
+++ Palendrome_code_challenge.py
@@ -6,3 +6,17 @@ for c in s if c.isalnum())
 print(is_palendrome("racecar"))
 print(is_palendrome("pirate"))
 print(is_palendrome("A man, a plan, a canal: Panama"))
+
+
+def is_palendrome1(teststr):
+  temp = teststr.lower() # Call the lower() method
+  newstr = ""
+  for c in temp:
+    if c.isalnum():
+      newstr += c
+  reversestr = newstr[::-1]
+  return reversestr == newstr # Move return statement outside the loop
+
+print(is_palendrome1("racecar"))
+print(is_palendrome1("pirate"))
+print(is_palendrome1("A man, a plan, a canal: Panama"))
